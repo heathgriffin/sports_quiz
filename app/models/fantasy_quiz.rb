d03e1cb1896872bb894cb4ac52addd0b68293186
Heath Griffin <heath@socialcompass.net>
2012-06-18 18:08:07 -0500

    updated stuff and things

--- app/models/fantasy_quiz.rb
+++ app/models/fantasy_quiz.rb
@@ -1,32 +1,32 @@
 class FantasyQuiz < ActiveRecord::Base
   attr_accessible :email, :name, :total_score, :question_one_answer, :question_two_answer, :question_one_score, :question_two_score
 
+  def computed_score
+    total_score = question_two_score + question_one_score
+  end
+
   def fantasy_land?
-    total_score <= 200
+    computed_score <= 200
   end
   
   def fantasy_wannabe?
-    total_score < 300
+    computed_score < 300
   end
   
   def fantasy_joe?
-    total_score < 400
+    computed_score < 400
   end
   
   def fantasy_expert?
-    total_score < 450
+    computed_score < 450
   end
   
   def fantasy_guru?
-    total_score < 500
+    computed_score < 500
   end
 
   def fantasy_god?
-    total_score >= 500
-  end
-
-  def computed_score
-    total_score = question_two_score + question_one_score
+    computed_score >= 500
   end
 
 end
